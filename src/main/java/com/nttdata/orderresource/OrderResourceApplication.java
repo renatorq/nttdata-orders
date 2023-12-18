4d45ebb99d95f7ebb801c2766b4596b2a16a3eb0
Pedro Rios <prios@canvia.com>
2023-12-17 20:22:57 -0500

    first commit

---
+++ src/main/java/com/nttdata/orderresource/OrderResourceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.orderresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/orderresource/OrderResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.orderresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
